6698877f006be78e02ba4a0ca2757050bb9fb758
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-18 00:16:18 +0530

    NewRepo

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterCom from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <FooterCom />
+    </BrowserRouter>
+  );
+}
+
+export default App;
