2bd4af9d9c308c0e4927d71c79c391f996d0a764
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-25 12:24:13 +0530

    profile update

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -33,23 +33,23 @@ export const updateUser = async (req, res, next) => {
 				errorHandler(400, "Username can only contain letters and numbers"),
 			);
 		}
-		try {
-			const updatedUser = await User.findByIdAndUpdate(
-				req.params.userId,
-				{
-					$set: {
-						username: req.body.username,
-						email: req.user.email,
-						profilePicture: req.body.profilePicture,
-						password: req.body.password,
-					},
+	}
+	try {
+		const updatedUser = await User.findByIdAndUpdate(
+			req.params.userId,
+			{
+				$set: {
+					username: req.body.username,
+					email: req.user.email,
+					profilePicture: req.body.profilePicture,
+					password: req.body.password,
 				},
-				{ new: true },
-			);
-			const { password, ...rest } = updatedUser._doc;
-			res.status(200).json(rest);
-		} catch (err) {
-			next(err);
-		}
+			},
+			{ new: true },
+		);
+		const { password, ...rest } = updatedUser._doc;
+		res.status(200).json(rest);
+	} catch (err) {
+		next(err);
 	}
 };
