1460ec1cf44b878ac3f10adfb626594748d85edc
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-25 21:39:22 +0530

    GetPOSTS API

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -20,7 +21,9 @@ const Dashboard = () => {
         <DashSidebar />
       </div>
       {/*Profile */}
-      {tab === 'profile' && <DashProfile /> }
+      {tab === "profile" && <DashProfile />}
+      {/*Posts */}
+      {tab === "posts" && <DashPosts />}
     </div>
   );
 };
