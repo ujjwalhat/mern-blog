4572e255958941975b0bbb13b43031ce3d2fa6ae
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-26 00:39:41 +0530

    Add DeleteUSER func

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -53,7 +53,22 @@ function DashUsers() {
     }
   };
 
-  const handleDeleteUser = async () => {};
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setShowModal(false);
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div
       className="table-auto overflow-x-auto 
