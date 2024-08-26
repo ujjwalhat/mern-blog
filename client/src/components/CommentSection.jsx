9ca8213155aac2a19e8930c2274c280f56a1d4e0
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-26 12:05:23 +0530

    CommentAPI

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import { Alert, Button, Textarea, TextInput } from "flowbite-react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) {
+      return;
+    }
+    try {
+      const res = await fetch("/api/comment/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: comment,
+          postId,
+          userId: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+      }
+    } catch (error) {
+      setCommentError(error.message);
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -22,10 +53,39 @@ function CommentSection({ postId }) {
           </Link>
         </div>
       ) : (
-        <div className="">
+        <div className="text-sm text-teal-500 my-5 flex gap-1">
           You must be signed in to comment
-          <Link to={"/sign-in"}>Sign In</Link>
+          <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
+            Sign In
+          </Link>
         </div>
+      )}
+      {currentUser && (
+        <form
+          className="border border-teal-500 rounded-md p-3"
+          onSubmit={handleSubmit}
+        >
+          <Textarea
+            placeholder="Add a comment..."
+            rows="3"
+            maxLength="200"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className="flex justify-between items-center mt-5 ">
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} Characters remaining
+            </p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
+          </div>
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
+        </form>
       )}
     </div>
   );
