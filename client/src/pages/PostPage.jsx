86b968d7d02b74673c94f3d890e76b74fda31bce
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-26 19:54:23 +0530

    Might get back

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,14 +1,16 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import CallToActions from "../components/CallToActions";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
+import CallToActions from "../components/CallToActions";
 
-function PostPage() {
+export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -33,6 +35,22 @@ function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch("/api/post/getposts?limit=3");
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -57,26 +75,30 @@ function PostPage() {
         alt={post?.title}
         className="mt-10 p-3 max-h-[600px] w-full object-cover"
       />
-      <div className="flex justify-between mx-auto w-full max-w-2xl text-xs p-3 border-b border-slate-500">
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className="italic">
-          {(post?.content.length / 1000).toFixed(0)} mins read
+          {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
-
-      {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
       <div
         className="p-3 max-w-2xl mx-auto w-full post-content"
         // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-        // biome-ignore lint/security/noDangerouslySetInnerHtmlWithChildren: <explanation>
         dangerouslySetInnerHTML={{ __html: post?.content }}
-      ></div>
+      />
       <div className="max-w-4xl mx-auto w-full">
         <CallToActions />
       </div>
       <CommentSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent articles</h1>
+        <div className="flex flex-wrap gap-3 mt-5 justify-center">
+          {recentPosts?.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
-
-export default PostPage;
