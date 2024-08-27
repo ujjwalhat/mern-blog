a1a18ff28168fcef978c5a53819dec78b6261dc7
ujjwalhat <ujjwal31072002@gmail.com>
2024-08-27 13:52:59 +0530

    Final Commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -99,6 +99,14 @@ function Header() {
                 {currentUser.email}
               </span>
             </Dropdown.Header>
+            {currentUser.isAdmin && (
+              <>
+                <Link to={"/dashboard?tab=dash"}>
+                  <Dropdown.Item>Dashboard</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider />
+              </>
+            )}
             <Link to={"/dashboard?tab=profile"}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
